337e331b952d7e4a2c630fdeeeeff04218730bfb
MaksKrasn <uw5ekr@gmail.com>
2020-06-28 19:22:21 +0300

    Добавлены пояснения

--- app/src/main/java/com/home/logintest/ui/login/LoginActivity.java
+++ app/src/main/java/com/home/logintest/ui/login/LoginActivity.java
@@ -38,7 +38,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        //dbIni();
+        dbIni();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
