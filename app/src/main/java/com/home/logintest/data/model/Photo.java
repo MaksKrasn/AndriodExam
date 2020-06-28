4253482cb32ee9f000b62fa9ba0478540547f1d3
MaksKrasn <uw5ekr@gmail.com>
2020-06-28 18:16:44 +0300

    Добавлен класс Photo и зависимости в build.gradle

---
+++ app/src/main/java/com/home/logintest/data/model/Photo.java
@@ -0,0 +1,31 @@
+package com.home.logintest.data.model;
+
+public class Photo {
+    private String photoPath;
+    private int authorId;
+    private String description;
+
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
